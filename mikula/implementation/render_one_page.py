35e53ec5dd1e977d82f28b9f6f3ec1b3b8de8d2c
Roman <kosobrodov@fastmail.fm>
2020-06-25 20:47:20 +1000

    rendering pages in theme One

--- mikula/implementation/render_one_page.py
+++ mikula/implementation/render_one_page.py
@@ -10,7 +10,7 @@ THUMBNAILS = settings.thumbnails_dir
 USER_ASSETS = settings.user_assets_dir
 
 
-def render_album_page(album, keys, index, template):
+def render_album_page(album, keys, index, template, page_list):
     gallery_root, child_albums, meta, content = parse_subdirectories(album, keys, index)
 
     path = ["."]
@@ -30,7 +30,8 @@ def render_album_page(album, keys, index, template):
                            keys_=keys,
                            path_=path,
                            index_=index,
-                           sources_=image_sources)
+                           sources_=image_sources,
+                           page_list_=page_list)
     return html
 
 
@@ -42,17 +43,18 @@ def render(album, error_page, pages, output_directory, theme, config):
     album_template = env.get_template("album.html")
     error_template = env.get_template("error.html")
 
-    keys = tuple(reversed(album.keys()))
-    for index in range(len(keys)):
-        album_page = render_album_page(album, keys, index, album_template)
-        dst_directory = os.path.join(output_directory, keys[index])
-        album_filename = os.path.join(dst_directory, "index.html")
-        with open(album_filename, 'w') as fid:
-            fid.write(album_page)
-
     page_list = list()
     if len(pages) > 0:
         pages_template = env.get_template("pages.html")
         page_list = render_pages(pages, output_directory, pages_template, config)
 
     create_page(error_page, page_list, output_directory, "error.html", error_template, config)
+
+    keys = tuple(reversed(album.keys()))
+    for index in range(len(keys)):
+        album_page = render_album_page(album, keys, index, album_template, page_list)
+        dst_directory = os.path.join(output_directory, keys[index])
+        album_filename = os.path.join(dst_directory, "index.html")
+        with open(album_filename, 'w') as fid:
+            fid.write(album_page)
+
